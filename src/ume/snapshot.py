8381b5daeb2d343558fee90a19cfa8fdc3c276e9
Tino Tonitini <georgepike0@gmail.com>
2025-06-04 11:36:02 -0400

    Clarify snapshot comment about edge data

--- src/ume/snapshot.py
+++ src/ume/snapshot.py
@@ -28,7 +28,7 @@ def snapshot_graph_to_file(graph: MockGraph, path: Union[str, pathlib.Path]) -> 
         IOError: If an error occurs during file writing.
         TypeError: If the data from graph.dump() is not JSON serializable.
     """
-    dumped_data = graph.dump() # This currently returns {"nodes": ...}
+    dumped_data = graph.dump()  # {"nodes": ..., "edges": ...}
     with open(path, "w", encoding='utf-8') as f:
         json.dump(dumped_data, f, indent=2)
 
