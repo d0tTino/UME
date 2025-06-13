7174a5c50ff8df70ec8189398dbb5cf8ceb11158
Tino Tonitini <georgepike0@gmail.com>
2025-06-13 17:18:04 -0400

    Ignore extra settings

--- src/ume/config.py
+++ src/ume/config.py
@@ -1,9 +1,10 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Extra
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+        env_file=".env", env_file_encoding="utf-8", extra=Extra.ignore
     )
 
     # UME Core
