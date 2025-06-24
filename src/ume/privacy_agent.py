3e62010e59b37468dac464eb2b55782e7b155936
Tino Tonitini <georgepike0@gmail.com>
2025-06-24 12:40:45 -0400

    Integrate alignment plugins and configurable policies

--- src/ume/privacy_agent.py
+++ src/ume/privacy_agent.py
@@ -3,6 +3,10 @@ from __future__ import annotations
 
 import warnings
 from .pipeline.privacy_agent import *  # noqa: F401,F403
+from .pipeline import privacy_agent as _pa
+
+_ANALYZER = _pa._ANALYZER
+_ANONYMIZER = _pa._ANONYMIZER
 
 warnings.warn(
     "ume.privacy_agent is deprecated; use ume.pipeline.privacy_agent instead.",
