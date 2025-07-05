d59d646ae78ecfc125b47b82e3d41b0821fe6fe6
Tino Tonitini <georgepike0@gmail.com>
2025-07-04 23:36:59 -0400

    Add MemGPT and SuperMemory integrations

--- src/integrations/__init__.py
+++ src/integrations/__init__.py
@@ -2,5 +2,7 @@
 
 from .langgraph import LangGraph
 from .letta import Letta
+from .memgpt import MemGPT
+from .supermemory import SuperMemory
 
-__all__ = ["LangGraph", "Letta"]
+__all__ = ["LangGraph", "Letta", "MemGPT", "SuperMemory"]
