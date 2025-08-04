b34fc690550fe064689335f00e8e4fd1604620ce
Tino Tonitini <georgepike0@gmail.com>
2025-08-04 18:41:39 -0400

    Skip tests when optional dependencies missing

--- src/ume/models/__init__.py
+++ src/ume/models/__init__.py
@@ -3,8 +3,11 @@
 from .users import User, create_user
 from .calendar import CalendarEvent, create_calendar_event
 from .calendar_layer import CalendarLayer, create_calendar_layer
+from .decision_analysis import DecisionAnalysis, create_decision_analysis
 from .decisions import Decision, create_decision
 from .finance import Transaction, create_transaction
+from .financial_account import FinancialAccount, create_financial_account
+from .proposed_action import ProposedAction, create_proposed_action
 from .user_group import UserGroup, create_user_group
 
 
@@ -19,9 +22,12 @@ __all__ = [
     "create_decision",
     "DecisionAnalysis",
     "create_decision_analysis",
+    "ProposedAction",
+    "create_proposed_action",
     "Transaction",
     "create_transaction",
+    "FinancialAccount",
+    "create_financial_account",
     "UserGroup",
     "create_user_group",
-
 ]
